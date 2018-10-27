7c40e0a7a160230bd1bb39c64710edf6a88af91e
nfmontenegro <nicolas.fmontenegro@gmail.com>
2018-10-27 19:42:35 -0300

    publication resolvers

--- server/src/graphql/resolvers/publication.resolver.js
+++ server/src/graphql/resolvers/publication.resolver.js
@@ -1,8 +1,9 @@
 export default {
   Query: {
-    listPublications: async (_, params, {models: {Publication}}) => {
-      return await Publication.find({}).populate('user')
-    }
+    listPublications: async (_, params, {models: {Publication}}) =>
+      await Publication.find({}).populate('user'),
+    listPublication: async (_, {_id}, {models: {Publication}}) =>
+      await Publication.findOne({_id}).populate('user')
   },
   Mutation: {
     createPublication: async (
@@ -12,18 +13,42 @@ export default {
     ) => {
       if (!userAccess) throw new Error(`Don't have permissions`)
 
-      const publication = await Publication.create({
+      const {_id} = await Publication.create({
         title,
         description,
         user
       })
 
-      const sample = await Publication.findOne({
-        title: 'Manchi Perdida'
-      }).populate('user')
+      return await Publication.findOne({_id}).populate('user')
+    },
+    updatePublication: async (
+      _,
+      {_id, userId, ...rest},
+      {models: {Publication}, request: {userAccess}}
+    ) => {
+      if (!userAccess) throw new Error(`Don't have permissions`)
+      const publication = await Publication.findOneAndUpdate(
+        {
+          _id,
+          user: userId
+        },
+        rest
+      )
 
-      console.log(sample)
-      return sample
+      if (!publication) throw new Error(`Can't update this publication`)
+
+      return await Publication.findOne({_id}).populate('user')
+    },
+    deletePublication: async (
+      _,
+      {_id},
+      {models: {Publication}, request: {userAccess}}
+    ) => {
+      if (!userAccess) throw new Error(`Don't have permissions`)
+
+      const publication = await Publication.findOneAndDelete({_id})
+      if (!publication) throw new Error(`No publication with that id: ${_id}`)
+      return `Publication removed ${_id}`
     }
   }
 }
